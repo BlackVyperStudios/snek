72fa3d5aea62f54baab6da4e18943919a24d80c3
Damon Leven <mcwertgaming@gmail.com>
2020-10-04 21:29:17 +0200

    added debug and release frontend

---
+++ src/frontend-release.cpp
@@ -0,0 +1,19 @@
+/*  =====================================
+ *  Release interface for SNEK
+ *  Created by MCWertGaming
+ *  Licensed under MIT on GitHub
+ *  https://github.com/MCWertGaming/snek
+ * ===================================== */
+#include "snake.h"
+
+int main()
+{
+    snake::snake snek(true, 20, 20);
+
+    unsigned short int quit = 0;
+    while (quit == 0)
+    {
+        quit = snek.update();
+    }
+    return 0;
+}
