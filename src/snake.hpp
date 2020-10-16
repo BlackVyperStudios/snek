e81eae1d725c0f39e0ab40cab896bd772b644b47
Damon Leven <mcwertgaming@gmail.com>
2020-10-16 15:32:25 +0200

    added comments

--- src/snake.hpp
+++ src/snake.hpp
@@ -6,27 +6,45 @@ namespace snake
     {
     private:
         /* game variables */
+        // stores the snakes position
         unsigned short int snakePos[2][100]{0};
+        // temporarily stores the next snake movement
         unsigned short int newSnakePos[2]{0};
+        // snakes length
         unsigned short int snakeLength = 1;
+        // stores the apple positions apple[<appleDefinition>][<X/Y>], please use the apple definition
         unsigned short int apple[2][2]{0};
+        // indicates, if the magenta apple exists currently
         bool magentaAppleExist = false;
+        // score
         unsigned short int score = 0;
+        // indicates the last direction the snake moved to, please use the definitions
         unsigned short int lastDir{};
+        // stores the input, please use the definitions
         short int input{};
+        // stores the calculated snake speed factor for generating the actual snake speed
         double snakeSpeedFactor{};
+        // stores the actual snake speed
         unsigned short int snakeSpeed{};
         /* game options */
+        // toggles if the player can move 'back', so backwards (which will end the game)
         const bool oppositeDir = false;
+        // minimum snake speed in micro seconds
         const unsigned short int minSpeed = 10;
+        // maximum snake speed in micro seconds
         const unsigned short int maxSpeed = 500;
+        // toggles the slowing of the up and down speed to make it more natural
         const bool movementFix = false;
+        // game screen size screen[X/Y]
         const unsigned short int screen[2];
         /* automatic options */
+        // toggles, if the console supports colors
         bool consoleSupportsColors = true;
 
         /* pre-game */
+        // places the snake to the default position
         void setDefaultPos();
+        // calculates the speed factor for generating the actual snake speed
         void calcSpeedFactor();
 
         /* drawing */
@@ -36,32 +54,41 @@ namespace snake
         void drawWatermark();
         void animateWatermark();
         void drawSnake();
+        // draws the apple drawApple(appleDefinition), please use the definitions
         void drawApple(unsigned short int);
 
-        /* game-object creation */
+        /* game-objects*/
+        // creates a new apple createApple(appleDefinition), please use the definitions
         void createApple(unsigned short int);
-
-        /* game-object updating */
+        // responsible for updating the apples (creating or adding them)
         void updateApple();
 
         /* checks */
+        // if true the given position is already in use or illegal illegalPosition(X,Y,snake?)
+        // the bool toggles, if places, where the apple could exists are illegal or not (and the opposite)
         bool illegalPosition(const unsigned short int*, const unsigned short int*, bool);
         bool redAppleEaten();
         bool magentaAppleEaten();
 
         /* game mechanics */
+        // measures the input and stores it
         void getInput();
+        // updates the snakePos[][]
         void updateSnakePos();
+        // calculates on what position the snake would be next
         void calcNewSnakePos();
+        // calculates the actual snake speed and stores it into snakeSpeed
         void increaseSnakeSpeed();
 
         /* fixes */
+        // slows left and right movement to make it feel more natural
         inline void normaliseMovementSpeed() const;
     public:
         // constructor for default game options
         explicit snake(bool, bool, unsigned short int, unsigned short int);
         // constructor for manual game options
-        snake(bool, bool, bool, unsigned short int, unsigned short int, bool, unsigned short int, unsigned short int, bool);
+        snake(bool, bool, bool, unsigned short int, unsigned short int, bool, unsigned short int, unsigned short int,
+              bool);
         // updates the game state
         unsigned short int update();
         // destructor destroys game and ncurses
