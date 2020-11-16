032bdf53b6d620e8b67ae6db2af70010b83d8ffb
Damon Leven <mcwertgaming@gmail.com>
2020-11-16 19:20:56 +0100

    added comments

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,18 @@
 
 int main()
 {
+    // try/catch to make sure the destructor of Term::Terminal gets called to reset the terminal state
     try
     {
-        // setup terminal library
-        Term::Terminal term(true,true);
-        term.save_screen();
+        // setup terminal library, CTRL+C will prevent Term::Terminal's destructor
+        Term::Terminal termObj(true,true);
+        termObj.save_screen();
         // turn off the cursor
         std::cout << Term::cursor_off();
 
+        // start the menu
         menu::menu menuObj;
-        menuObj.start(&term);
-
+        menuObj.start(&termObj);
 
         // make sure the terminal is reverted to its original state
         std::cout << Term::color(Term::style::reset)
@@ -21,16 +22,17 @@ int main()
                   << Term::color(Term::bg::reset)
                   << Term::cursor_on();
     }
+    // cpp-terminal will throw a runtime error on exceptions
     catch(const std::runtime_error& re)
     {
         std::cerr << "Runtime error: " << re.what() << std::endl;
         return 1;
     }
+    // catch unknown errors
     catch (...)
     {
         std::cerr << "Unknown error." << std::endl;
         return 2;
     }
-
     return 0;
 }
