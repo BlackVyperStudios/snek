1f8ba0b947ac5f1a5222a72599cadf2b202f80ed
Damon Leven <mcwertgaming@gmail.com>
2021-03-03 21:03:49 +0000

    removed unsused stuff

--- .ci/compile.sh
+++ .ci/compile.sh
@@ -6,11 +6,6 @@ CLEANUP()
     git reset --hard
     git clean -fdx
 }
-RUN_PROGRAMMS()
-{
-    # a list of all programs / targets compiled by this project
-    # currently there are no programms that could run by semselves
-}
 
 # compile gcc release
 export CC=/usr/bin/gcc
@@ -18,7 +13,6 @@ export CXX=/usr/bin/g++
 CLEANUP
 cmake -DCMAKE_INSTALL_PREFIX=./inst .
 cmake --build . --config Release
-RUN_PROGRAMMS
 
 # compile gcc debug
 export CC=/usr/bin/gcc
@@ -26,7 +20,6 @@ export CXX=/usr/bin/g++
 CLEANUP
 cmake -DCMAKE_INSTALL_PREFIX=./inst .
 cmake --build . --config Debug
-RUN_PROGRAMMS
 
 # compile clang release
 export CC=/usr/bin/clang
@@ -34,7 +27,6 @@ export CXX=/usr/bin/clang++
 CLEANUP
 cmake -DCMAKE_INSTALL_PREFIX=./inst .
 cmake --build . --config Release
-RUN_PROGRAMMS
 
 # compile clang debug
 export CC=/usr/bin/clang
@@ -42,4 +34,3 @@ export CXX=/usr/bin/clang++
 CLEANUP
 cmake -DCMAKE_INSTALL_PREFIX=./inst .
 cmake --build . --config Debug
-RUN_PROGRAMMS
